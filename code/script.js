ce4d195b4abf16c02375f96bb313b17a66db4ef1
Evan Brock <evanbrock@gmail.com>
2020-12-14 00:04:22 -0500

    added prev/next hover and arrow key navigation

--- code/script.js
+++ code/script.js
@@ -1,3 +1,5 @@
+/* SCROLL TO TOP */
+
 $(document).ready(function(){
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
@@ -22,3 +24,28 @@ $(document).ready(function(){
       } // End if
     });
   });
+
+  /* PREV AND NEXT WITH ARROW KEYS */
+
+  // when the document is ready, run this function
+jQuery(function( $ ) {
+    var keymap = {};
+
+    // LEFT
+    keymap[ 37 ] = "#prev";
+    // RIGHT
+    keymap[ 39 ] = "#next";
+
+    $( document ).on( "keyup", function(event) {
+        var href,
+            selector = keymap[ event.which ];
+        // if the key pressed was in our map, check for the href
+        if ( selector ) {
+            href = $( selector ).attr( "href" );
+            if ( href ) {
+                // navigate where the link points
+                window.location = href;
+            }
+        }
+    });
+});
